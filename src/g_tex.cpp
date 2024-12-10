1415aa3b3efb49a5c97e3595699a21054e37079e
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2024-12-10 11:48:55 +1100

    texture: Static constant not global
    Static constant not global.

--- src/g_tex.cpp
+++ src/g_tex.cpp
@@ -133,7 +133,7 @@ static const uint8_t s_png_crcsize = 4;
 //------------------------------------------------------------------------------
 // Table of CRCs
 //------------------------------------------------------------------------------
-static unsigned long g_crc_table[256] = {
+static const unsigned long s_crc_table[256] = {
     0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
     0xe963a535, 0x9e6495a3, 0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
     0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2,
@@ -191,7 +191,7 @@ unsigned long UpdateCrc(unsigned long crc, unsigned char* buf, int len)
 
     for (int n = 0; n < len; n++)
     {
-        c = g_crc_table[(c ^ buf[n]) & 0xFF] ^ (c >> 8);
+        c = s_crc_table[(c ^ buf[n]) & 0xFF] ^ (c >> 8);
     }
 
     return c;
